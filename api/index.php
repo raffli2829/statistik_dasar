21b133da2b46b2650268b2c86eaebbb5f2b27c86
Raffli2829 <tamamraffli@gmail.com>
2026-09-24 11:38:38 +0700

    fix3.2

--- api/index.php
+++ api/index.php
@@ -1,3 +1,23 @@
 <?php
 
+// Buat berkas database SQLite di direktori ephemeral Vercel jika belum ada
+$databasePath = '/tmp/database.sqlite';
+if (!file_exists($databasePath)) {
+    touch($databasePath);
+}
+
+// Buat direktori storage yang dibutuhkan Laravel
+$storageFolders = [
+    '/tmp/storage/framework/views',
+    '/tmp/storage/framework/sessions',
+    '/tmp/storage/framework/cache',
+    '/tmp/storage/logs',
+];
+
+foreach ($storageFolders as $folder) {
+    if (!is_dir($folder)) {
+        mkdir($folder, 0755, true);
+    }
+}
+
 require __DIR__ . '/../public/index.php';
